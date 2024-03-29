dc76bb3d176c79be929c050c25dd047fc487af06
taeaeaexin <re_fuse@naver.com>
2024-03-29 16:11:05 +0900

    BXMA29(1610).not

---
+++ Chap_06_TwoDimensionalArray/_01_B3_2738_matrixAddition.java
@@ -0,0 +1,32 @@
+package Chap_06_TwoDimensionalArray;
+
+import java.util.Scanner;
+
+public class _01_B3_2738_matrixAddition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int A[][] = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++)
+                A[i][j] = sc.nextInt();
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++)
+                A[i][j] += sc.nextInt();
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                System.out.print(A[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+
+            
+    }
+}
