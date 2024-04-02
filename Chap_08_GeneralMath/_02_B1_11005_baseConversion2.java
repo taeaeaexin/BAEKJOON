77736d7b7f0d77af221c86fdf92e79e63c3c0220
taeaeaexin <re_fuse@naver.com>
2024-04-02 17:44:26 +0900

    아진짜이게맞냐

--- Chap_08_GeneralMath/_02_B1_11005_baseConversion2.java
+++ Chap_08_GeneralMath/_02_B1_11005_baseConversion2.java
@@ -3,5 +3,6 @@ package Chap_08_GeneralMath;
 public class _02_B1_11005_baseConversion2 {
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println();
     }
 }
