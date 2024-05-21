72be8b28e98976f2f7f4336c95bb6b567de702b2
문태신 <re_fuse@naver.com>
2024-05-21 18:45:08 +0900

    Create _11_S2_18870_zipxy.java

---
+++ Chap_13_Sort/_11_S2_18870_zipxy.java
@@ -0,0 +1,18 @@
+package Chap_13_Sort;
+
+import jdk.nashorn.internal.runtime.regexp.joni.ScanEnvironment;
+
+import java.util.Scanner;
+
+public class _11_S2_18870_zipxy {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int arr[] = new int[N+1];
+
+        for (int i = 1; i <= N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+    }
+}
